d01dfc4c5a3c22ec7f5a1c2d8a040a2722346a37
jaswanth82006 <jaswanth82006@gmail.com>
2025-11-29 01:25:32 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api-maatramkk360.vercel.app/api',
+    baseURL: 'https://api-maatramkk360.onrender.com/api',
 });
 
 export default api;
